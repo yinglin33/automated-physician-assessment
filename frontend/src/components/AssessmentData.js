38fa3d7b9d85e694757a2a9f440c97a9aaad46a2
Kelly Ye <kellyye@Kellys-MacBook-Pro-2.local>
2022-10-16 10:56:28 -0700

    made changes to frontend

---
+++ frontend/src/components/AssessmentData.js
@@ -0,0 +1,28 @@
+const AssessmentData = {
+    "assessments": {
+        "entries": [
+            {
+                'date': '10/16/2022 14:00',
+                'name': 'Kelly Ye',
+            },
+            {
+                'date': '10/15/2022 14:00',
+                'name': 'Kelly Ye'
+            },
+            {
+                'date': '10/14/2022 14:00',
+                'name': 'Kelly Ye'
+            },
+            {
+                'date': '10/13/2022 14:00',
+                'name': 'Kelly Ye'
+            },
+            {
+                'date': '10/12/2022 14:00',
+                'name': 'Kelly Ye'
+            }
+        ]
+    }
+}
+
+export default AssessmentData
---
+++ frontend/src/components/Entry.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Entry() {
+    return (
+        <div>
+            <h1> Hello World! </h1>
+            <div class='back-button'> Back </div>
+
+            <h1> Pradeep Mani Rathnam's Exam Score</h1>
+            <h3> Date: 10/15/2022</h3>
+            <h3> Administered By: Kelly Ye</h3>
+
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Entry;
